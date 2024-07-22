98cbeec57c863210d3ecffb919da152313ec9a77
tellmebaby <tellmebaby407@gmail.com>
2024-07-22 11:48:11 +0900

    first

---
+++ app/lib/main.dart
@@ -0,0 +1,76 @@
+import 'package:app/screen/board/insert_screen.dart';
+import 'package:app/screen/board/list_screen.dart';
+import 'package:app/screen/board/read_screen.dart';
+import 'package:app/screen/board/update_screen.dart';
+import 'package:app/screen/main_screen.dart';
+import 'package:flutter/material.dart';
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      initialRoute: '/main',
+      routes: {
+        '/main' : (context) => const MainScreen(),
+        '/board/list' : (context) => const ListScreen(),
+        '/board/read' : (context) => const ReadScreen(),
+        '/board/insert' : (context) => const InsertScreen(),
+        '/board/update' : (context) => const UpdateScreen(),
+      },
+    );
+  }
+}
+
+
+
+
+
+// import 'package:flutter/material.dart';
+// import 'package:http_app/screen/board/insert_screen.dart';
+// import 'package:http_app/screen/board/list_screen.dart';
+// import 'package:http_app/screen/board/read_screen.dart';
+// import 'package:http_app/screen/board/update_screen.dart';
+// import 'package:http_app/screen/main_screen.dart';
+
+// void main() {
+//   runApp(const MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+    
+//     return MaterialApp(
+//         title: 'Flutter Demo',
+//         theme: ThemeData(
+//           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+//           useMaterial3: true,
+//         ),
+//         initialRoute: '/main',
+//         routes: {
+//           '/main': (context) => MainScreen(),
+//           '/board/list': (context) => ListScreen(),
+//           '/board/read': (context) => ReadScreen(),
+//           '/board/insert': (context) => InsertScreen(),
+//           '/board/update': (context) => UpdateScreen(),
+//         }
+//         // home: const MyHomePage(title: 'Flutter Demo Home Page'),
+//         );
+//   }
+// }
---
+++ app/lib/models/board.dart
@@ -0,0 +1,16 @@
+class Board {
+  int? no;
+  String? title;
+  String? writer;
+  String? content;
+  DateTime? regDate;
+  DateTime? updDate;
+
+  Board(
+      {required this.no,
+      required this.title,
+      required this.writer,
+      required this.content,
+      this.regDate,
+      this.updDate});
+}
